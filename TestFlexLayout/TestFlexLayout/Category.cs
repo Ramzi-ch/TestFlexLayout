88ff229c8768805edfa0e767fc811337add2d234
Ramzi Chakroun <r.chakroun@root.sa>
2018-04-16 09:28:18 +0100

    Add project files.

---
+++ TestFlexLayout/TestFlexLayout/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestFlexLayout
+{
+    public class Category
+    {
+        public String Image { get; set; }
+        public String Code { get; set; }
+        public String NameAr { get; set; }
+        public String NameEn { get; set; }
+        public String ShortNameAr { get; set; }
+        public String ShortNameEn { get; set; }
+
+    }
+}
---
+++ TestFlexLayout/TestFlexLayout/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestFlexLayout
+{
+	public partial class MainPage : ContentPage
+	{
+        List<Category> ListCategory;
+        public MainPage()
+		{            
+            InitializeComponent(); 
+            ListCategory = new List<Category>
+            {
+                new Category{Code="0000",Image="gallery.png", NameAr="Name Arabic",NameEn="Name En",ShortNameAr="ShortAr",ShortNameEn="ShortEn"},
+                new Category{Code="0000",Image="gallery.png", NameAr="Name Arabic",NameEn="Name En",ShortNameAr="ShortAr",ShortNameEn="ShortEn"},
+                new Category{Code="0000",Image="gallery.png", NameAr="Name Arabic",NameEn="Name En",ShortNameAr="ShortAr",ShortNameEn="ShortEn"}
+            };
+            ListCategorie.ItemsSource = ListCategory;
+            this.SizeChanged += OnSizeChanged;
+        }       
+
+        void OnSizeChanged(object sender, EventArgs e)
+        {
+
+            Debug.WriteLine(this.Width +" "+ this.Height);
+            //Debug.WriteLine("width " + L5.Width);
+            //if (L5.Width > 0)
+            //{
+            //    st1.HeightRequest = L5.Width - 40;
+            //}
+
+        }
+    }
+}
